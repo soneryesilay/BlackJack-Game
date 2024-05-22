b1e5bc7b061d50493af5141dc4b3bf422235023a
soneryesilay <soneryesilay@outlook.com>
2024-05-22 14:15:27 +0300

    first commit

---
+++ blackjack.js
@@ -0,0 +1,160 @@
+var dealerSum = 0;
+var yourSum = 0;
+
+var dealerAceCount = 0;
+var yourAceCount = 0;
+
+var hidden;
+var deck = [];
+
+var canHit = true;
+
+window.onload = function() {
+    buildDeck();
+    shuffleDeck();
+    startGame();
+}
+
+function buildDeck() {
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let types = ["C", "D", "H", "S"];
+    deck = [];
+
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + "-" + types[i]);
+        }
+    }
+    console.log(deck);
+}
+
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+    console.log(deck);
+}
+
+function startGame() {
+    dealerSum = 0;
+    yourSum = 0;
+    dealerAceCount = 0;
+    yourAceCount = 0;
+    canHit = true;
+
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+    dealerAceCount += checkAce(hidden);
+
+    while (dealerSum < 17) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
+        dealerSum += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
+    }
+    console.log(dealerSum);
+
+    for (let i = 0; i < 2; i++) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
+        yourSum += getValue(card);
+        yourAceCount += checkAce(card);
+        document.getElementById("your-cards").append(cardImg);
+    }
+
+    console.log(yourSum);
+    document.getElementById("hit").addEventListener("click", hit);
+    document.getElementById("stay").addEventListener("click", stay);
+    document.getElementById("reset").addEventListener("click", resetGame);
+}
+
+function hit() {
+    if (!canHit) {
+        return;
+    }
+
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./cards/" + card + ".png";
+    yourSum += getValue(card);
+    yourAceCount += checkAce(card);
+    document.getElementById("your-cards").append(cardImg);
+
+    if (reduceAce(yourSum, yourAceCount) > 21) {
+        canHit = false;
+        document.getElementById("results").innerText = "Kaybettiniz";
+        revealDealerCard(); 
+        document.getElementById("your-sum").innerText = yourSum;
+        document.getElementById("dealer-sum").innerText = dealerSum;
+
+    }
+}
+
+function stay() {
+    dealerSum = reduceAce(dealerSum, dealerAceCount);
+    yourSum = reduceAce(yourSum, yourAceCount);
+
+    canHit = false;
+    revealDealerCard();
+
+    let message = "";
+    if (yourSum > 21) {
+        message = "Kaybettiniz";
+    } else if (dealerSum > 21) {
+        message = "Kazandınız";
+    } else if (yourSum == dealerSum) {
+        message = "Berabere";
+    } else if (yourSum > dealerSum) {
+        message = "Kazandınız";
+    } else {
+        message = "Kaybettiniz";
+    }
+
+    document.getElementById("dealer-sum").innerText = dealerSum;
+    document.getElementById("your-sum").innerText = yourSum;
+    document.getElementById("results").innerText = message;
+}
+
+function revealDealerCard() {
+    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+}
+
+function resetGame() {
+    location.reload();
+
+}
+
+function getValue(card) {
+    let data = card.split("-");
+    let value = data[0];
+
+    if (isNaN(value)) {
+        if (value == "A") {
+            return 11;
+        }
+        return 10;
+    }
+
+    return parseInt(value);
+}
+
+function checkAce(card) {
+    if (card[0] == "A") {
+        return 1;
+    }
+    return 0;
+}
+
+function reduceAce(playerSum, playerAceCount) {
+    while (playerSum > 21 && playerAceCount > 0) {
+        playerSum -= 10;
+        playerAceCount -= 1;
+    }
+    return playerSum;
+}
